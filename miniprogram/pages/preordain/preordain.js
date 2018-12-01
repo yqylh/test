6d9d85966aa197b958ea23a4ae029b735eb6b875
Xyher <1598214674@qq.com>
2018-12-01 15:26:13 +0800

    add pro

--- miniprogram/pages/preordain/preordain.js
+++ miniprogram/pages/preordain/preordain.js
@@ -2,8 +2,30 @@
 //获取应用实例
 var app = getApp()
 const date = new Date()
+const years = []
+const months = []
+const days = []
+for (let i = 1990; i <= date.getFullYear(); i++) {
+  years.push(i)
+}
+
+for (let i = 1; i <= 12; i++) {
+  months.push(i)
+}
+
+for (let i = 1; i <= 31; i++) {
+  days.push(i)
+}
+
 Page({
   data: {
+    year: date.getFullYear(),
+    month:date.getMonth(),
+    day: date.getDate(), 
+    years: years,
+    months: months,
+    days: days,
+    value: [0, 0, 0],                                                        
     background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
     indicatorDots: true,
     vertical: false,
@@ -16,11 +38,72 @@ Page({
     open: [false, true, true],
     ordList: ['test1', 'test2\n换行'],
     waitList: ['waiting one', 'waiting two', 'waiting tree'],
-    hisList: ['his1', 'his2']
+    hisList: ['his1', 'his2'],
+    showModalStatus: false,
   },
-  intervalChange: function (e) {
+ 
+  /**
+   * 对话框确认按钮点击事件
+   */
+
+  powerDrawer: function (e) {
+    var currentStatu = e.currentTarget.dataset.statu;
+    this.util(currentStatu)
+  },
+  util: function (currentStatu) {
+    /* 动画部分 */
+    // 第1步：创建动画实例 
+    var animation = wx.createAnimation({
+      duration: 200,  //动画时长
+      timingFunction: "linear", //线性
+      delay: 0  //0则不延迟
+    });
+
+    // 第2步：这个动画实例赋给当前的动画实例
+    this.animation = animation;
+
+    // 第3步：执行第一组动画：Y轴偏移240px后(盒子高度是240px)，停
+    animation.translateY(240).step();
+
+    // 第4步：导出动画对象赋给数据对象储存
     this.setData({
-      interval: e.detail.value
+      animationData: animation.export()
+    })
+
+    // 第5步：设置定时器到指定时候后，执行第二组动画
+    setTimeout(function () {
+      // 执行第二组动画：Y轴不偏移，停
+      animation.translateY(0).step()
+      // 给数据对象储存的第一组动画，更替为执行完第二组动画的动画对象
+      this.setData({
+        animationData: animation
+      })
+
+      //关闭抽屉
+      if (currentStatu == "close") {
+        this.setData(
+          {
+            showModalStatus: false
+          }
+        );
+      }
+    }.bind(this), 200)
+
+    // 显示抽屉
+    if (currentStatu == "open") {
+      this.setData(
+        {
+          showModalStatus: true
+        }
+      );
+    }
+  },
+   bindChange: function (e) {
+    const val = e.detail.value
+    this.setData({
+      year: this.data.years[val[0]],
+      month: this.data.months[val[1]],
+      day: this.data.days[val[2]]
     })
   }
 })
