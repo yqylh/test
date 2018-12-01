2c2eaa3428cd53b0593487086cbd02d1f5d6dd53
yqylh <1071665725@qq.com>
2018-12-01 14:49:06 +0800

    my note all

--- miniprogram/pages/clientInfo/clientInfo.js
+++ miniprogram/pages/clientInfo/clientInfo.js
@@ -2,32 +2,39 @@
 var id
 wx.cloud.init()
 const db = wx.cloud.database()
+const _ = db.command
 Page({
-
-  data: {
-    hiddenmodalput: true,
-    //可以通过hidden是否掩藏弹出框的属性，来指定那个弹出框
-  },
-  formBindsubmit: function (e) {
+  onLoad: function() {
     wx.cloud.callFunction({
       name: 'login',
       complete: res => {
         id = res.result.OPENID
       }
     })
-    const _ = db.command
+  },
+  data: {
+    hiddenmodalput: true,
+    pwd:'nihao',
+    userName: '1',
+    userNum: '1'
+    //可以通过hidden是否掩藏弹出框的属性，来指定那个弹出框
+  },
+  submit: function (e) {
+    var nname = this.data.userName
+    var nnum = this.data.userNum
+    // console.log(this.data.userName)
     db.collection('user').where({
       _openid: _.eq(id)
     })
       .get({
         success: function (res) {
+          // console.log(e.detail.value.userName)
           db.collection('user').doc(res.data[0]._id).update({
-          data: {
-            name: e.detail.value.userName,
-            usernum: e.detail.value.userNum
-          },
+            data: {
+              name: nname,
+              usernum: nnum
+            },
           })
-          
           wx.switchTab({
             url: '../my/my',
           })
@@ -35,27 +42,83 @@ Page({
             title: '提交成功',
           })
         }
+      })
+  },
+  getname: function (e) {
+    this.setData({
+      userName: e.detail.value
     })
+  },
+  getnum: function (e) {
+    this.setData({
+      userNum: e.detail.value
+    })
+  },
+  getmi:function(e) {
+    this.setData({
+      pwd:e.detail.value
+    })
+  },
+  showDialogBtn: function () {
 
+    this.setData({
+      showModal: true
+    })
   },
   /**
-   * 组件的方法列表
+   * 弹出框蒙层截断touchmove事件
    */
-    modalinput: function () {
-      this.setData({
-        hiddenmodalput: !this.data.hiddenmodalput
+  preventTouchMove: function () {
+  },
+  /**
+   * 隐藏模态对话框
+   */
+  hideModal: function () {
+    this.setData({
+      showModal: false
+    });
+  },
+  /**
+   * 对话框取消按钮点击事件
+   */
+  onCancel: function () {
+    this.hideModal();
+  },
+  /**
+   * 对话框确认按钮点击事件
+   */
+  onConfirm: function () {
+    var truepwd = "sddaqdxqn3hys"
+    if (truepwd == this.data.pwd) {
+      db.collection('user').where({
+        _openid: _.eq(id)
       })
-    },
-    //取消按钮
-    cancel: function () {
-      this.setData({
-        hiddenmodalput: true
-      });
-    },
-    //确认
-    confirm: function () {
-      this.setData({
-        hiddenmodalput: true
+      .get({
+        success: function (res) {
+          // console.log(e.detail.value.userName)
+          db.collection('user').doc(res.data[0]._id).update({
+            data: {
+              Identity: "Admin",
+            }
+          })
+          wx.switchTab({
+            url: '../my/my',
+          })
+          wx.showToast({
+            title: '申请成功',
+            icon: 'success',
+            duration: 2000
+          })
+        }
       })
+    } else {
+      wx.showToast({
+        title: '申请失败',
+        icon: 'none',
+        duration: 2000
+      })
+    }
+
+    this.hideModal()
   }
 })
