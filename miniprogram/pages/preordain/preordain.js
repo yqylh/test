933b4264ccfcce4d78b3cc264594f4b3b28b145c
e <1598214674@qq.com>
2018-12-01 20:22:01 +0800

    modify files

--- miniprogram/pages/preordain/preordain.js
+++ miniprogram/pages/preordain/preordain.js
@@ -1,11 +1,49 @@
 // pages/logs/logs.js
 //获取应用实例
 var app = getApp()
+var b1
+var b2
+var b3
 const date = new Date()
-const years = []
-const months = []
-const days = []
-for (let i = 1990; i <= date.getFullYear(); i++) {
+var startyear;
+startyear= date.getFullYear();
+var s2 
+s2= date.getMonth()+1;
+var s3
+s3 = date.getDate();
+var ed1;
+var ed2;
+var ed3;
+var va;
+var years = []
+var months = []
+var days = []
+if(s2==2)
+{
+  if (startyear % 400 == 0 || (startyear % 4 == 0 && startyear%100==0))
+  {
+    va=29;
+  }
+  va=28;
+}else{
+  if (s2 == 1 || s2 == 3 || s2 == 5 || s2 == 7 || s2 == 8 || s2 == 10 || s2 ==12) va=31;
+  else va=30;
+}
+ed3=s3+30;
+ed2=s2;
+while(ed3>va)
+{
+  ed3-=va;
+  ed2++;
+}
+if(ed3==0)ed3=va;
+ed1 = startyear;
+if(ed2>12)
+{
+  ed1++;
+  ed2-=12;
+}
+for (let i = startyear; i <= ed1; i++) {
   years.push(i)
 }
 
@@ -16,12 +54,18 @@ for (let i = 1; i <= 12; i++) {
 for (let i = 1; i <= 31; i++) {
   days.push(i)
 }
-
 Page({
   data: {
-    year: date.getFullYear(),
-    month:date.getMonth(),
-    day: date.getDate(), 
+    datename:"日期",
+    year: startyear,
+    month:s2,
+    day: s3, 
+    a1:startyear,
+    a2:s2,
+    a3:s3,
+    eyear: ed1,
+    emonth: ed2,
+    eday: ed3, 
     years: years,
     months: months,
     days: days,
@@ -45,10 +89,36 @@ Page({
   /**
    * 对话框确认按钮点击事件
    */
-
-  powerDrawer: function (e) {
+  powerDrawer:function(e){
     var currentStatu = e.currentTarget.dataset.statu;
     this.util(currentStatu)
+  },
+  powerDrawer1: function (e) {
+    if (((b1 < ed1) || (b1 == ed1 && b2 < ed2) || (b1 == ed1 && b2 == ed2 && b3 <= ed3)) && ((b1 > startyear || (b1 == startyear && b2 > s2) || (b1 == startyear && b2 == s2 && b3 >= s3))))
+    {
+        var currentStatu = e.currentTarget.dataset.statu;
+        this.util(currentStatu);
+        this.setData({
+          datename: b1 + '.' + b2 + '.' + b3
+        })
+        wx.showToast({
+          title: '成功',
+        })
+    }
+    else{
+        wx.showModal({
+          title: '提示',
+          content: '日期不在规定范围内',
+          success: function (res) {
+            if (res.confirm) {
+              console.log('用户点击重置')
+            } else {
+              console.log('用户点击取消')
+            }
+
+          }
+        })
+    }
   },
   util: function (currentStatu) {
     /* 动画部分 */
@@ -100,6 +170,9 @@ Page({
   },
    bindChange: function (e) {
     const val = e.detail.value
+     b1=this.data.years[val[0]]
+     b2=this.data.months[val[1]]
+     b3=this.data.days[val[2]]
     this.setData({
       year: this.data.years[val[0]],
       month: this.data.months[val[1]],
