0a315f7545067da332be74865ad691823cbdabe9
Eric-Whyte <982680793@qq.com>
2018-12-02 13:37:29 +0800

    fffffinal

--- miniprogram/pages/apply/apply.js
+++ miniprogram/pages/apply/apply.js
@@ -10,10 +10,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-    meetingTheme:'',
-    meetingContent:'',
-    mediaNeeded:false,
-    teaNeed:false
+    meetingTheme: '',
+    meetingContent: '',
+    mediaNeeded: false,
+    teaNeed: false
   },
 
   /**
@@ -77,39 +77,39 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-      
+
   },
-  getTheme:function(e){
+  getTheme: function (e) {
     this.setData({
-      meetingTheme:e.detail.value,
+      meetingTheme: e.detail.value,
     })
   },
   getCont: function (e) {
-      this.setData({
-        meetingContent: e.detail.value,
-      })
+    this.setData({
+      meetingContent: e.detail.value,
+    })
   },
   switch1Change: function (e) {
     this.setData({
-      mediaNeeded:e.detail.value,
+      mediaNeeded: e.detail.value,
     })
   },
   switch2Change: function (e) {
     this.setData({
-      teaNeed:e.detail.value,
+      teaNeed: e.detail.value,
     })
   },              ///将该四个变量传到数据库中即可
-  confirm:function(e){
+  confirm: function (e) {
     var Mythis = this
     var where_id = e.id
     // console.log(time)
-    if(this.data.meetingTheme!=''){
+    if (this.data.meetingTheme != '') {
       var date2 = new Date;
       var date22 = date2.toString()
       db.collection('meeting').add({
         data: {
           Introduction: Mythis.data.meetingContent,
-          check:2,
+          check: 2,
           time: date22,
           where: where_id,
           title: Mythis.data.meetingTheme,
@@ -128,7 +128,7 @@ Page({
             success: function (res) {
               db.collection('user').doc(res._id).update({
                 data: {
-                  participate:_.push(date22)
+                  participate: _.push(date22)
                 }
               })
             }
@@ -136,14 +136,14 @@ Page({
       wx.navigateBack({
         url: '../index/index',
       }),
-      wx.showToast({
-        title: '申请成功!',
-        icon: 'success',
-      })
-    }else {
+        wx.showToast({
+          title: '申请成功!',
+          icon: 'success',
+        })
+    } else {
       wx.showToast({
         title: '会议主题不能为空!',
-        image:'../../images/关闭.png',
+        image: '../../images/关闭.png',
       })
     }
   }
